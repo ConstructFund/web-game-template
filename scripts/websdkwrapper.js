725d0f5d1d36d7463c1e938c9ebb984d329fe504
Ossama <skymen75019@gmail.com>
2022-11-06 15:18:42 +0100

    Fixed an issue with deepcopy

--- scripts/websdkwrapper.js
+++ scripts/websdkwrapper.js
@@ -1,54 +1,77 @@
-runOnStartup(async runtime => {
-	globalThis.skymenExexJSOnDom = runtime.objects.ExecuteJsOnDom.getFirstInstance().sdkInst;
-	const execJs = globalThis.skymenExexJSOnDom;
-	
-	execJs.CreateFunction("deepCopyObjWithoutFunctions", ["obj"], `
+runOnStartup(async (runtime) => {
+  globalThis.skymenExexJSOnDom =
+    runtime.objects.ExecuteJsOnDom.getFirstInstance().sdkInst;
+  const execJs = globalThis.skymenExexJSOnDom;
+
+  execJs.CreateFunction(
+    "deepCopyObjWithoutFunctions",
+    ["obj", "filter"],
+    `
 		if (obj === null || typeof obj !== 'object') {
 			return obj;
 		}
-
 		var temp = obj.constructor(); // give temp the original obj's constructor
+		if (temp === null || temp === undefined) {
+			temp = {};
+		}
 		for (var key in obj) {
-			if (typeof obj[key] === 'function') {
+			if (typeof obj[key] === 'function' || filter.some(x=>obj[key] === x)) {
 				continue;
 			}
 			temp[key] = globalThis.skymenExexJSOnDom._OnCallFunction({"name": "deepCopyObjWithoutFunctions", "args": [obj[key]]})
 		}
 		return temp;
-	`)
-	
-	execJs.CreateFunction("handleInit", ["args"], `
+	`
+  );
+
+  execJs.CreateFunction(
+    "handleInit",
+    ["args"],
+    `
 		let [name, debug = false, data = {}] = args;
 		globalThis.WebSdkWrapper.init(name, debug, data).then(() => {
-			let currentSdk
+			let currentSdk = globalThis.WebSdkWrapper.currentSdk;
 			globalThis.WebSdkWrapper.dispatch("onInit", {
 			  enabled: globalThis.WebSdkWrapper.enabled,
 			  hasAdblock: globalThis.WebSdkWrapper.hasAdblock(),
-			  currentSdk: globalThis.skymenExexJSOnDom._OnCallFunction({"name": "deepCopyObjWithoutFunctions", "args": [globalThis.WebSdkWrapper.currentSdk]}),
+			  currentSdk: globalThis.skymenExexJSOnDom._OnCallFunction({"name": "deepCopyObjWithoutFunctions", "args": [globalThis.WebSdkWrapper.currentSdk, [currentSdk.sdk]]}),
 			})
 		}).catch(e => {
 			globalThis.WebSdkWrapper.dispatch("onInit", {err: e})
 		})
-	`);
-	
-	execJs._DispatchToDom = (function (e) {
-		if (execJs.ignoreDispatch) return;
-		execJs.PostToDOM("dispatch", e);
-	})
-	execJs._OnDispatch = (function (e) {
-		execJs.ignoreDispatch = true;
-		globalThis.WebSdkWrapper.dispatch(...e);
-		execJs.ignoreDispatch = false;
-	})
-	execJs.AddDOMMessageHandler("dispatch", e=>execJs._OnDispatch(e))
-	
-	globalThis.WebSdkWrapper.onPause(() => {runtime.callFunction("M_Monetisation_OnPause")});
-	globalThis.WebSdkWrapper.onResume(() => {runtime.callFunction("M_Monetisation_OnResume")});
-	globalThis.WebSdkWrapper.onMute(() => {runtime.callFunction("M_Monetisation_OnMute")});
-	globalThis.WebSdkWrapper.onUnmute(() => {runtime.callFunction("M_Monetisation_OnUnmute")});
-	globalThis.WebSdkWrapper.onUnlockAllLevels(() => {runtime.callFunction("M_Monetisation_OnUnlockAllLevels")});
-	globalThis.WebSdkWrapper.onAdStarted(() => {runtime.callFunction("M_Monetisation_OnAdStarted")});
-})
+	`
+  );
+
+  execJs._DispatchToDom = function (e) {
+    if (execJs.ignoreDispatch) return;
+    execJs.PostToDOM("dispatch", e);
+  };
+  execJs._OnDispatch = function (e) {
+    execJs.ignoreDispatch = true;
+    globalThis.WebSdkWrapper.dispatch(...e);
+    execJs.ignoreDispatch = false;
+  };
+  execJs.AddDOMMessageHandler("dispatch", (e) => execJs._OnDispatch(e));
+
+  globalThis.WebSdkWrapper.onPause(() => {
+    runtime.callFunction("M_Monetisation_OnPause");
+  });
+  globalThis.WebSdkWrapper.onResume(() => {
+    runtime.callFunction("M_Monetisation_OnResume");
+  });
+  globalThis.WebSdkWrapper.onMute(() => {
+    runtime.callFunction("M_Monetisation_OnMute");
+  });
+  globalThis.WebSdkWrapper.onUnmute(() => {
+    runtime.callFunction("M_Monetisation_OnUnmute");
+  });
+  globalThis.WebSdkWrapper.onUnlockAllLevels(() => {
+    runtime.callFunction("M_Monetisation_OnUnlockAllLevels");
+  });
+  globalThis.WebSdkWrapper.onAdStarted(() => {
+    runtime.callFunction("M_Monetisation_OnAdStarted");
+  });
+});
 
 globalThis.WebSdkWrapper = (function () {
   /*
@@ -58,23 +81,23 @@ globalThis.WebSdkWrapper = (function () {
   const events = {};
 
   function listen(event, fn, { once = false } = {}) {
-	events[event] = events[event] || [];
-	events[event].push({
-	  fn,
-	  once,
-	});
+    events[event] = events[event] || [];
+    events[event].push({
+      fn,
+      once,
+    });
   }
 
   function listenOnce(event, fn) {
-	listen(event, fn, { once: true });
+    listen(event, fn, { once: true });
   }
 
   function dispatch(event, ...data) {
-	(events[event] || []).forEach((fnObj) => {
-	  fnObj.fn(...data);
-	});
-	events[event] = (events[event] || []).filter((fnObj) => !fnObj.once);
-	globalThis.skymenExexJSOnDom._DispatchToDom([event, ...data]);
+    (events[event] || []).forEach((fnObj) => {
+      fnObj.fn(...data);
+    });
+    events[event] = (events[event] || []).filter((fnObj) => !fnObj.once);
+    globalThis.skymenExexJSOnDom._DispatchToDom([event, ...data]);
   }
   /*
   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -86,90 +109,93 @@ globalThis.WebSdkWrapper = (function () {
   let enabled = false;
   let hasAdblock = false;
   const Wrapper = {
-	get enabled() {
-	  return enabled;
-	},
-	get currentSdk() {
-	  return currentSdk;
-	},
-	async init(name, debug = false, data = {}) {
-	  return new Promise((resolve, reject) => {
-		listenOnce("onInit", (e) => {
-		  if (e.err) reject(e.err)
-		  else {
-		  	enabled = e.enabled;
-			hasAdblock = e.hasAdblock;
-			currentSdk = e.currentSdk;
-			resolve(e);
-		  }
-		});
-		globalThis.skymenExexJSOnDom.CallFunction("handleInit", [[name, debug, data]]);
-	  }) 
-	},
-	onPause(fn) {
-	  listen("pause", fn);
-	},
-	pause() {
-	  dispatch("pause");
-	},
-	onResume(fn) {
-	  listen("resume", fn);
-	},
-	resume() {
-	  dispatch("resume");
-	},
-	onMute(fn) {
-	  listen("mute", fn);
-	},
-	mute() {
-	  dispatch("mute");
-	},
-	onUnmute(fn) {
-	  listen("unmute", fn);
-	},
-	unmute() {
-	  dispatch("unmute");
-	},
-	onUnlockAllLevels(fn) {
-	  listen("unlockAllLevels", fn);
-	  globalThis.skymenExexJSOnDom.Eval(`globalThis.WebSdkWrapper.onUnlockAllLevels(() => {
+    get enabled() {
+      return enabled;
+    },
+    get currentSdk() {
+      return currentSdk;
+    },
+    async init(name, debug = false, data = {}) {
+      return new Promise((resolve, reject) => {
+        listenOnce("onInit", (e) => {
+          if (e.err) reject(e.err);
+          else {
+            enabled = e.enabled;
+            hasAdblock = e.hasAdblock;
+            currentSdk = e.currentSdk;
+            resolve(e);
+          }
+        });
+        globalThis.skymenExexJSOnDom.CallFunction("handleInit", [
+          [name, debug, data],
+        ]);
+      });
+    },
+    onPause(fn) {
+      listen("pause", fn);
+    },
+    pause() {
+      dispatch("pause");
+    },
+    onResume(fn) {
+      listen("resume", fn);
+    },
+    resume() {
+      dispatch("resume");
+    },
+    onMute(fn) {
+      listen("mute", fn);
+    },
+    mute() {
+      dispatch("mute");
+    },
+    onUnmute(fn) {
+      listen("unmute", fn);
+    },
+    unmute() {
+      dispatch("unmute");
+    },
+    onUnlockAllLevels(fn) {
+      listen("unlockAllLevels", fn);
+      globalThis.skymenExexJSOnDom
+        .Eval(`globalThis.WebSdkWrapper.onUnlockAllLevels(() => {
 		globalThis.WebSdkWrapper.dispatch("unlockAllLevels");
 	  })`);
-	},
-	hasAdblock() {
-	  return hasAdblock;
-	},
-	loadingStart() {
-	  dispatch("loadingStart");
-	},
-	loadingProgress(progress) {
-	  progress = Math.min(Math.max(0, progress), 100);
-	  dispatch("loadingProgress", progress);
-	},
-	loadingEnd() {
-	  dispatch("loadingEnd");
-	},
-	gameplayStart() {
-	  dispatch("gameplayStart");
-	},
-	gameplayStop() {
-	  dispatch("gameplayStop");
-	},
-	happyTime() {
-	  dispatch("happyTime");
-	},
-	levelStart(level) {
-	  dispatch("levelStart", level);
-	},
-	replayLevel(level) {
-	  dispatch("replayLevel", level);
-	},
-	score(score) {
-	  dispatch("score", score);
-	},
-	banner(data) {
-	  dispatch("banner", data);
-	},
+    },
+    hasAdblock() {
+      return hasAdblock;
+    },
+    loadingStart() {
+      dispatch("loadingStart");
+    },
+    loadingProgress(progress) {
+      progress = Math.min(Math.max(0, progress), 100);
+      dispatch("loadingProgress", progress);
+    },
+    loadingEnd() {
+      dispatch("loadingEnd");
+    },
+    gameplayStart() {
+      dispatch("gameplayStart");
+    },
+    gameplayStop() {
+      dispatch("gameplayStop");
+    },
+    happyTime() {
+      dispatch("happyTime");
+    },
+    levelStart(level) {
+      dispatch("levelStart", level);
+    },
+    replayLevel(level) {
+      dispatch("replayLevel", level);
+    },
+    score(score) {
+      dispatch("score", score);
+    },
+    banner(data) {
+      dispatch("banner", data);
+    },
     interstitial(handleGameplayStart = false) {
       sdkContext.lastRequestedAd = "interstitial";
       if (!currentSdk || !currentSdk.hasAds) {
@@ -182,7 +208,7 @@ globalThis.WebSdkWrapper = (function () {
         listenOnce("adStarted", () => {
           if (handleGameplayStart && gameplayStarted) Wrapper.gameplayStop();
           Wrapper.mute();
-        })
+        });
         listenOnce("interstitialEnd", (...args) => {
           if (handleGameplayStart && gameplayStarted) Wrapper.gameplayStart();
           Wrapper.unmute();
@@ -203,7 +229,7 @@ globalThis.WebSdkWrapper = (function () {
         listenOnce("adStarted", () => {
           if (handleGameplayStart && gameplayStarted) Wrapper.gameplayStop();
           Wrapper.mute();
-        })
+        });
         listenOnce("rewardedEnd", (...args) => {
           if (handleGameplayStart && gameplayStarted) Wrapper.gameplayStart();
           Wrapper.unmute();
@@ -212,15 +238,15 @@ globalThis.WebSdkWrapper = (function () {
         dispatch("rewarded");
       });
     },
-	onAdStarted(fn) {
-	  listen("adStarted", fn);
-	},
-	hasAds() {
-	  return currentSdk && currentSdk.hasAds;
-	},
-	listen,
-	listenOnce,
-	dispatch
+    onAdStarted(fn) {
+      listen("adStarted", fn);
+    },
+    hasAds() {
+      return currentSdk && currentSdk.hasAds;
+    },
+    listen,
+    listenOnce,
+    dispatch,
   };
   return Wrapper;
 })();
